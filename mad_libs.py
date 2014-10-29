be4c58717a18b719d0f5bae675fa0a020a1ad442
Mary Smia <mary.e.sima@gmail.com>
2014-10-28 21:44:03 -0400

    adding third project

---
+++ mad_libs.py
@@ -0,0 +1,8 @@
+noun1 = raw_input("Choose a noun: ")
+adj1 = raw_input("Choose an adjective: ")
+verb1 = raw_input("Choose a verb: ")
+noun2 = raw_input("Choose a noun: ")
+adj2 = raw_input("Choose an adjective: ")
+verb2 = raw_input("Choose a verb: ")
+
+print "There once was a {}. He lived in a {} igloo. He {}ed every single day for fun. Most of all, he enjoyed taking {}s and {} food and {} all over town." .format(noun1,adj1,verb1,noun2,adj2,verb2)	
